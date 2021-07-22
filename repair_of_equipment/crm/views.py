9993123c3fe84e30f97f10e701838b46421c2633
wiky-avis <peta-holding@yandex.ru>
2021-07-22 18:04:45 +0300

    исправлены пути к файлам

--- repair_of_equipment/crm/views.py
+++ repair_of_equipment/crm/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user_model
+from .models import Request
 
 User = get_user_model()
 
@@ -13,5 +14,6 @@ def index(request):
 
 def profile_employee(request, username):
     employee = get_object_or_404(User, username=username)
+    requests = Request.objects.all()
     return render(
-        request, 'profile_employee.html', {'employee': employee})
+        request, 'mailbox.html', {'employee': employee, 'requests': requests.count()})
